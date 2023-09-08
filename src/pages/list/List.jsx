ad8017cab52ff2a9357d2573caca8bbfe33dcc2f
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-09-08 12:55:20 +0700

    first commit

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,13 @@
+import "./list.scss"
+
+const List = () => {
+  return (
+    <div className='home'>
+        <h1 className="title">
+            Bookspot Admin
+        </h1>
+    </div>
+  )
+}
+
+export default List
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,13 @@
+import "./new.scss"
+
+const New = () => {
+  return (
+    <div className='home'>
+        <h1 className="title">
+            Bookspot Admin
+        </h1>
+    </div>
+  )
+}
+
+export default New
